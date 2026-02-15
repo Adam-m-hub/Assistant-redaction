63025aa4f2c14a2333dbb4f09026710e546fe44c
Adams <a.mahamataboubakar0031@uca.ac.ma>
2026-02-15 14:40:13 +0000

    Ajout support PWA pour fonctionnement hors-ligne

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB
-      },
-      devOptions: {
-        enabled: false
-      }
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
